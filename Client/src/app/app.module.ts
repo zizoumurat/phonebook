c6a8855c9d03c088e5f8912d3868eef768d6d15e
Murat Dere <muratdere90@gmail.com>
2023-12-28 08:56:47 +0300

    remove-unused-statc-files

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
-import { LoggerModule } from 'ngx-logger';
-import { environment } from '../environments/environment';
 
 import { ServiceModule } from './service/service.module';
 import { DataModule } from './data/data.module';
@@ -21,12 +19,6 @@ import { PresentationModule } from './presentation/presentation.module';
     BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule,
-    LoggerModule.forRoot({
-      serverLoggingUrl: `http://my-api/logs`,
-      level: environment.logLevel,
-      serverLogLevel: environment.serverLogLevel
-    }),
-
     DataModule,
     ServiceModule,
     PresentationModule,
