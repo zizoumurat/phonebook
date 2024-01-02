de40951115e295cc61ba634d7f3a59537842113d
Murat Dere <muratdere90@gmail.com>
2024-01-02 13:05:49 +0300

    folder-structure-edited

---
+++ backend/PhoneBook/Services/ReportsService/test/ReportsService.UnitTest/Services/ReportServiceUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+using ReportsService.Domain.Repositories;
+
+namespace ReportsService.UnitTest.Services
+{
+    public class ReportServiceUnitTest
+    {
+        private readonly Mock<IReportRepository> _personService;
+
+        public ReportServiceUnitTest()
+        {
+            _personService = new();
+        }
+
+    }
+}
