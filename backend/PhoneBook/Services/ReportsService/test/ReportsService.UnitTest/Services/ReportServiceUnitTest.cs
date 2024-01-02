9be4a758d907b7684df7b2564c520f14e28b8d91
Murat Dere <muratdere90@gmail.com>
2024-01-02 21:11:12 +0300

    complated-tests

--- backend/PhoneBook/Services/ReportsService/test/ReportsService.UnitTest/Services/ReportServiceUnitTest.cs
+++ backend/PhoneBook/Services/ReportsService/test/ReportsService.UnitTest/Services/ReportServiceUnitTest.cs
@@ -4,20 +4,19 @@ using ReportsService.Application.Services;
 using ReportsService.Domain.Dtos;
 using ReportsService.Domain.Entities;
 using ReportsService.Domain.Repositories;
+using ReportsService.UnitTest.Base;
 
 namespace ReportsService.UnitTest.Services
 {
-    public class ReportServiceUnitTest
+    public class ReportServiceUnitTest : BaseFixture
     {
         private Mock<IReportRepository> _reportRepository;
-        private Mock<IMapper> _mapperMock;
         private ReportService _reportService;
 
         public ReportServiceUnitTest()
         {
             _reportRepository = new Mock<IReportRepository>();
-            _mapperMock = new Mock<IMapper>();
-            _reportService = new ReportService(_reportRepository.Object, _mapperMock.Object);
+            _reportService = new ReportService(_reportRepository.Object, _mapper.Object);
         }
 
         [Fact]
@@ -26,7 +25,7 @@ namespace ReportsService.UnitTest.Services
             var reportId = 1;
             var detailList = new List<ReportDetailDto>();
 
-            _mapperMock.Setup(m => m.Map<List<ReportDetail>>(detailList)).Returns(new List<ReportDetail>());
+            _mapper.Setup(m => m.Map<List<ReportDetail>>(detailList)).Returns(new List<ReportDetail>());
 
             await _reportService.AddDetailList(reportId, detailList);
 
@@ -37,14 +36,14 @@ namespace ReportsService.UnitTest.Services
         public async Task Create_ShouldCallRepositoryWithMappedReport()
         {
             var reportDto = new ReportDto(); 
-            var mappedReport = new Report(); 
+            var mappedReport = new Report();
 
-            _mapperMock.Setup(m => m.Map<Report>(reportDto)).Returns(mappedReport);
+            _mapper.Setup(m => m.Map<Report>(reportDto)).Returns(mappedReport);
             _reportRepository.Setup(r => r.Create(mappedReport)).ReturnsAsync(mappedReport);
 
             var result = await _reportService.Create(reportDto);
-            
-            _mapperMock.Verify(m => m.Map<ReportDto>(mappedReport), Times.Once);
+
+            _mapper.Verify(m => m.Map<ReportDto>(mappedReport), Times.Once);
             _reportRepository.Verify(r => r.Create(mappedReport), Times.Once);
             mappedReport.Equals(result);
         }
